99e8d6da00f6a6fb8ad4da83d71ef625aa7d9e13
Benjamin Akrong <akrongwork@gmail.com>
2022-06-07 20:19:02 +0100

    Authentication complete for localstorage

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,21 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  email = '';
+  password = '';
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
-  gotosignup(){
-    this.router.navigate(['/signup']);  // define your component where you want to go
-}
-
-  ngOnInit(): void {
+  gotosignup() {
+    this.router.navigate(['/signup']); // define your component where you want to go
   }
 
+  ngOnInit(): void {}
+
+  onSubmit() {
+    //  first import the database from localStorage
+    const dataSource = localStorage.getItem('database');
+
+    // check if it is empty, if it is the alert that there is no such account
+    // if it is not empty then loop through it and find the account matching with the credentials
+    if (dataSource) {
+      const database = JSON.parse(dataSource);
+      const userFound = database.filter((data: any) => {
+        return data.email === this.email && data.password === this.password
+      });
+
+      if (userFound.length > 0) {
+        alert('User found');
+        console.log({ User: userFound[0] });
+      }
+    } else {
+      alert('User account not found');
+    }
+  }
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router'
+import { Router } from '@angular/router';
 // import { ApicallService } from '../apicall.service';
 import { LoginComponent } from '../login/login.component';
 // import { Users } from '../users';
@@ -8,22 +8,76 @@ import { LoginComponent } from '../login/login.component';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
- 
-  constructor(
-    private router: Router,
-    public http: HttpClient,
-  ) { }
- 
+  firstname = '';
+  lastname = '';
+  email = '';
+  number = '';
+  password = '';
+  confirm_password = '';
+  code = '';
+
+  constructor(private router: Router, public http: HttpClient) {}
 
   ngOnInit(): void {
     // this.getUserList();
   }
 
-  gotologin(){
+  gotologin() {
     this.router.navigate(['/login']);
   }
-  
+
+  hasError(event: any) {
+    console.log({ hasError: event });
+  }
+  getNumber(event: any) {
+    this.number = event;
+  }
+
+
+  onSubmit() {
+    if (this.password === this.confirm_password) {
+      const dataSource = localStorage.getItem('database');
+      if (dataSource) {
+        const database = JSON.parse(dataSource);
+        database.push({
+          firstName: this.firstname,
+          lastName: this.lastname,
+          email: this.email,
+          phone: this.number,
+          password: this.password,
+          code: this.code,
+         });
+        localStorage.setItem('database', JSON.stringify(database));
+        this.clearForm();
+      } else {
+        let database = [];
+        database.push({
+          firstName: this.firstname,
+          lastName: this.lastname,
+          email: this.email,
+          phone: this.number,
+          password: this.password,
+          code: this.code,
+        });
+        localStorage.setItem('database', JSON.stringify(database));
+        this.clearForm();
+      }
+      } else {
+        alert('Both passwords must be the same');
+      return;
+    }
+  }
+
+  clearForm() {
+    this.firstname = '';
+    this.lastname = '';
+    this.email = '';
+    this.number = '';
+    this.password = '';
+    this.confirm_password = '';
+    this.code = '';
+  }
 }
